f06795c5a28b70f75079150b7cd0e0781e8c3616
khadiza21 <bibikhadiza474@gmail.com>
2022-05-23 02:36:51 +0600

    firebase setup

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,14 +1,13 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQBMOPjoug-A_OySdPMMYqjCE_v6iKH4Y",
-  authDomain: "manufacturer-website-46972.firebaseapp.com",
-  projectId: "manufacturer-website-46972",
-  storageBucket: "manufacturer-website-46972.appspot.com",
-  messagingSenderId: "571834800453",
-  appId: "1:571834800453:web:e36fe65df0898ab8b1862c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
